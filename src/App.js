bed2ef1fe96694f8ccfd1ee45f430aa7f970bde9
Victor Borshchov <victor.borshchov@gmail.com>
2018-05-30 23:28:38 +0300

    event handlers

--- src/App.js
+++ src/App.js
@@ -3,14 +3,61 @@ import './App.css';
 import Person from './Person/Person';
 
 class App extends Component {
+  state = {
+    persons: [
+      { name: 'Vitya', age: 33 },
+      { name: 'Olesya', age: 28 },
+      { name: 'Alex', age: 4 }
+    ]
+  }
+
+  switchNameHandler = (newName) => {
+    this.setState({
+      persons: [
+        { name: newName, age: 33 },
+        { name: 'Olesya', age: 29 },
+        { name: 'Oleksiy', age: 4 }   
+      ]
+    })
+  }
+
+  namedChangedHandler = (event) => {
+    this.setState({
+      persons: [
+        { name: 'Vitya', age: 33 },
+        { name: event.target.value, age: 29 },
+        { name: 'Oleksiy', age: 4 }   
+      ]
+    })
+  }
+
   render() {
+    const style = {
+      backgroundColor: 'white',
+      font: 'inherit',
+      border: '1px solid blue',
+      padding: '8px',
+      cursor: 'pointer'
+    };
+
     return (
       <div className="App">
         <h1>Hi, I'm a Ract App!</h1>
         <p>This is really working!</p>
-        <Person />
-        <Person />
-        <Person />
+        <button
+          style={style}
+          onClick={() => this.switchNameHandler('Victor')}>Switch Name</button>
+        <Person
+          name={this.state.persons[0].name}
+          age={this.state.persons[0].age} />
+        <Person 
+          name={this.state.persons[1].name}
+          age={this.state.persons[1].age}
+          changed={this.namedChangedHandler} />
+        <Person
+          name={this.state.persons[2].name}
+          age={this.state.persons[2].age}
+          click={this.switchNameHandler.bind(this, 'Vitya!')} >My Hobbies: Racing</Person>
       </div>
     );
     // return React.createElement('div', {className: "App"}, React.createElement('h1', null, 'Does this works now?'))
--- src/Person/Person.js
+++ src/Person/Person.js
@@ -1,7 +1,14 @@
 import React from 'react';
+import './Person.css'
 
-const person = () => {
-    return <p>I'm a person and I am {Math.floor(Math.random() * 30)}</p>
+const person = ( props ) => {
+    return (
+        <div className="Person">
+            <p onClick={props.click}>I'm a {props.name} and I am {props.age} years old.</p>
+            <p>{props.children}</p>
+            <input type="text" onChange={props.changed} value={props.name}/>
+        </div>    
+    )
 }
 
 export default person
