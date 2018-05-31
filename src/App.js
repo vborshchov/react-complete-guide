4ae236ed379a54a268eef9c30627a8ddea95393e
Victor Borshchov <victor.borshchov@gmail.com>
2018-05-31 09:43:57 +0300

    fix typo

--- src/App.js
+++ src/App.js
@@ -42,7 +42,7 @@ class App extends Component {
 
     return (
       <div className="App">
-        <h1>Hi, I'm a Ract App!</h1>
+        <h1>Hi, I'm a React App!</h1>
         <p>This is really working!</p>
         <button
           style={style}
