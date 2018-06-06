8677976157e29c878464c07319fbb2436c3cfe06
Victor Borshchov <victor.borshchov@gmail.com>
2018-06-06 20:46:49 +0300

    styling final

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import React, { Component } from 'react';
-import './App.css';
+import classes from './App.css';
 import Person from './Person/Person';
 
 class App extends Component {
@@ -9,85 +9,81 @@ class App extends Component {
       { id: 'a685d72f-34bf-54b6-913c-1e2fa0039862 ', name: 'Olesya', age: 28 },
       { id: 'd61b927e-cbe1-5b93-996d-a567e2ed2bd9 ', name: 'Alex', age: 4 }
     ],
+    otherState: 'some other value',
     showPersons: false
   }
 
-  namedChangedHandler = (event, id) => {
-    const personIndex = this.state.persons.findIndex(p => {
-      return p.id === id
-    })
+  nameChangedHandler = ( event, id ) => {
+    const personIndex = this.state.persons.findIndex( p => {
+      return p.id === id;
+    } );
 
     const person = {
       ...this.state.persons[personIndex]
-    }
+    };
+
+    // const person = Object.assign({}, this.state.persons[personIndex]);
 
     person.name = event.target.value;
 
     const persons = [...this.state.persons];
-
     persons[personIndex] = person;
-    this.setState({ persons: persons })
+
+    this.setState( { persons: persons } );
   }
 
-  deletePersonHandler = (personIndex) => {
-    const persons = [...this.state.persons]
-    persons.splice(personIndex, 1)
-    this.setState({persons: persons})
+  deletePersonHandler = ( personIndex ) => {
+    // const persons = this.state.persons.slice();
+    const persons = [...this.state.persons];
+    persons.splice( personIndex, 1 );
+    this.setState( { persons: persons } );
   }
 
   togglePersonsHandler = () => {
     const doesShow = this.state.showPersons;
-    this.setState({showPersons: !doesShow})
+    this.setState( { showPersons: !doesShow } );
   }
 
-  render() {
-    const style = {
-      backgroundColor: 'green',
-      color: 'white',
-      font: 'inherit',
-      border: '1px solid blue',
-      padding: '8px',
-      cursor: 'pointer'
-    };
+  render () {
+    let persons = null;
+    let btnClass = '';
 
-    let persons = null
-    if (this.state.showPersons) {
+    if ( this.state.showPersons ) {
       persons = (
         <div>
-          {this.state.persons.map((person, index) => {
+          {this.state.persons.map( ( person, index ) => {
             return <Person
-              click={() => this.deletePersonHandler(index)}
+              click={() => this.deletePersonHandler( index )}
               name={person.name}
               age={person.age}
               key={person.id}
-              changed={(event) => this.namedChangedHandler(event, person.id)} />
-          })}
+              changed={( event ) => this.nameChangedHandler( event, person.id )} />
+          } )}
         </div>
       );
 
-      style.backgroundColor = 'red';
+      btnClass = classes.Red;
     }
 
-    let clasess = []
-
-    if (this.state.persons.length <= 2) {
-      clasess.push('red')
+    const assignedClasses = [];
+    if ( this.state.persons.length <= 2 ) {
+      assignedClasses.push( classes.red ); // classes = ['red']
     }
-    if (this.state.persons.length <= 1) {
-      clasess.push('bold')
+    if ( this.state.persons.length <= 1 ) {
+      assignedClasses.push( classes.bold ); // classes = ['red', 'bold']
     }
 
     return (
-      <div className="App">
-        <h1>Hi, I'm a Ract App!</h1>
-        <p className={clasess.join(' ')}>This is really working!</p>
+        <div className={classes.App}>
+          <h1>Hi, I'm a React App</h1>
+          <p className={assignedClasses.join( ' ' )}>This is really working!</p>
         <button
-          style={style}
+            className={btnClass}
           onClick={this.togglePersonsHandler}>Toggle Persons</button>
         {persons}
       </div>
     );
-    // return React.createElement('div', {className: "App"}, React.createElement('h1', null, 'Does this works now?'))
+    // return React.createElement('div', {className: 'App'}, React.createElement('h1', null, 'Does this work now?'));
   }
 }
 
